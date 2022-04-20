775cd400f2eaa467f42f57349aa656c34b20f82b
badhesha <badhesha3@gmail.com>
2022-04-19 23:39:05 -0700

    finish exam-end

--- lab_4.py
+++ lab_4.py
@@ -29,3 +29,22 @@ ask
     print to the user "You have used up your allotment of guesses."
     print the correct answer "The correct answer is 'Sacromento.'"
 """
+
+def main():
+    question = "What is the capital of California?"
+    answer = "Sacramento"
+    ask(question, answer)
+
+def ask(question, answer, max_tries=3):
+    tries = 0
+    ans = ""
+    while tries < max_tries:
+        tries += 1
+        ans = input(question)
+        if ans == answer:
+            print("Correct!")
+            break
+    if ans != answer:
+        print("You have used up your allotment of guesses.")
+
+main()
